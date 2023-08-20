38329e6bce16a7668aee17b0ed8408a02cf66751
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2023-08-20 12:05:49 -0700

    [ME-1822,ME-1823,ME-1824] Service Config for Connector Managed Sockets (#46)

--- types/service/http_service_configuration.go
+++ types/service/http_service_configuration.go
@@ -1,7 +1,33 @@
 package service
 
+const (
+	// HttpServiceTypeStandard is the http
+	// service type for standard http services.
+	HttpServiceTypeStandard = "standard"
+
+	// HttpServiceTypeConnectorFileServer is the http service
+	// type for the connector's built-in file webserver.
+	HttpServiceTypeConnectorFileServer = "connector_file_server"
+)
+
 // HttpServiceConfiguration represents service
 // configuration for http services (fka sockets).
 type HttpServiceConfiguration struct {
+	HttpServiceType string `json:"http_service_type"`
+
+	// mutually exclusive fields below
+	StandardHttpServiceConfiguration   *StandardHttpServiceConfiguration   `json:"standard_http_service_configuration,omitempty"`
+	FileServerHttpServiceConfiguration *FileServerHttpServiceConfiguration `json:"fileserver_http_service_configuration,omitempty"`
+}
+
+// StandardHttpServiceConfiguration represents service
+// configuration for standard http services (fka sockets).
+type StandardHttpServiceConfiguration struct {
 	HostnameAndPort // inherited
+}
+
+// FileServerHttpServiceConfiguration represents service
+// configuration for the connector built-in file webserver.
+type FileServerHttpServiceConfiguration struct {
+	TopLevelDirectory string `json:"top_level_directory,omitempty"`
 }
--- types/service/tls_service_configuration.go
+++ types/service/tls_service_configuration.go
@@ -1,7 +1,45 @@
 package service
 
+const (
+	// TlsServiceTypeStandard is the tls
+	// service type for standard tls services.
+	TlsServiceTypeStandard = "standard"
+
+	// TlsServiceTypeVpn is the tls service
+	// type for the connector's built-in vpn.
+	TlsServiceTypeVpn = "vpn"
+
+	// TlsServiceTypeHttpProxy is the tls service type
+	// for the connector's built-in http (forward) proxy.
+	TlsServiceTypeHttpProxy = "http_proxy"
+)
+
 // TlsServiceConfiguration represents service
 // configuration for tls services (fka sockets).
 type TlsServiceConfiguration struct {
+	TlsServiceType string `json:"tls_service_type,omitempty"`
+
+	// mutually exclusive fields below
+	StandardTlsServiceConfiguration  *StandardTlsServiceConfiguration  `json:"standard_tls_service_configuration,omitempty"`
+	VpnTlsServiceConfiguration       *VpnTlsServiceConfiguration       `json:"vpn_tls_service_configuration,omitempty"`
+	HttpProxyTlsServiceConfiguration *HttpProxyTlsServiceConfiguration `json:"http_proxy_tls_service_configuration,omitempty"`
+}
+
+// StandardTlsServiceConfiguration represents service
+// configuration for standard tls services (fka sockets).
+type StandardTlsServiceConfiguration struct {
 	HostnameAndPort // inherited
+}
+
+// VpnTlsServiceConfiguration represents service
+// configuration for vpn services services over the tls socket.
+type VpnTlsServiceConfiguration struct {
+	VpnSubnet string   `json:"vpn_subnet"`
+	Routes    []string `json:"routes,omitempty"`
+}
+
+// HttpProxyTlsServiceConfiguration represents service
+// configuration for http proxy services over the tls socket.
+type HttpProxyTlsServiceConfiguration struct {
+	HostAllowlist []string `json:"host_allowlist,omitempty"`
 }
