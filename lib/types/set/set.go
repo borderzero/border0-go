f6f374a029eb85140208ab8fffea1c6a5f745fe0
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2023-08-23 13:32:11 -0700

    [MISC] Add Concurrency Safe Set Implementation

--- lib/types/set/set.go
+++ lib/types/set/set.go
@@ -1,53 +1,11 @@
 package set
 
 // Set represents a set of unique elements
-type Set[T comparable] map[T]struct{}
-
-// New returns a new set
-func New[T comparable](ss ...T) Set[T] {
-	return make(Set[T]).Add(ss...)
-}
-
-// Has returns true if an element is in a set
-func (s Set[T]) Has(e T) bool {
-	_, ok := s[e]
-	return ok
-}
-
-// Add adds a list of elements to a set
-func (s Set[T]) Add(ss ...T) Set[T] {
-	for _, e := range ss {
-		s[e] = struct{}{}
-	}
-	return s
-}
-
-// Remove removes a list of elements from a set
-func (s Set[T]) Remove(ss ...T) Set[T] {
-	for _, e := range ss {
-		delete(s, e)
-	}
-	return s
-}
-
-// Join joins two sets
-func (s Set[T]) Join(ss Set[T]) Set[T] {
-	for k := range ss {
-		s[k] = struct{}{}
-	}
-	return s
-}
-
-// Copy returns a copy of a set
-func (s Set[T]) Copy() Set[T] {
-	return New[T]().Join(s)
-}
-
-// Slice returns the set as a slice
-func (s Set[T]) Slice() []T {
-	slice := []T{}
-	for k := range s {
-		slice = append(slice, k)
-	}
-	return slice
+type Set[T comparable] interface {
+	Has(T) bool
+	Add(...T) Set[T]
+	Remove(...T) Set[T]
+	Join(Set[T]) Set[T]
+	Copy() Set[T]
+	Slice() []T
 }
