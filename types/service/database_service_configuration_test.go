9df60d6f5dc91321bd205e757aa64f9371ec6981
Bas Toonk <bas@toonk.nl>
2024-02-01 11:49:35 +0100

    fix(ME-2180): fix cloudsql configuration

--- types/service/database_service_configuration_test.go
+++ types/service/database_service_configuration_test.go
@@ -47,23 +47,18 @@ func Test_DatabaseServiceConfiguration_Validate(t *testing.T) {
 		},
 	}
 	testGcpCloudSqlConfig := &GcpCloudSqlDatabaseServiceConfiguration{
-		CloudSqlConnectorEnabled: false,
-		Standard: &GcpCloudSqlStandardConfiguration{
-			DatabaseProtocol: DatabaseProtocolMySql,
-			HostnameAndPort: HostnameAndPort{
-				Hostname: "hostname",
-				Port:     3306,
+		HostnameAndPort: HostnameAndPort{
+			Hostname: "hostname",
+			Port:     3306,
+		},
+		TlsAuth: &DatabaseTlsAuthConfiguration{
+			UsernameAndPassword: UsernameAndPassword{
+				Username: "username",
+				Password: "password",
 			},
-			AuthenticationType: DatabaseAuthenticationTypeTls,
-			TlsAuth: &DatabaseTlsAuthConfiguration{
-				UsernameAndPassword: UsernameAndPassword{
-					Username: "username",
-					Password: "password",
-				},
-				TlsConfig: TlsConfig{
-					Certificate: "certificate",
-					Key:         "private-key",
-				},
+			TlsConfig: TlsConfig{
+				Certificate: "certificate",
+				Key:         "private-key",
 			},
 		},
 	}
@@ -447,35 +442,24 @@ func Test_AwsRdsDatabaseServiceConfiguration_Validate(t *testing.T) {
 func Test_GcpCloudSqlDatabaseServiceConfiguration_Validate(t *testing.T) {
 	t.Parallel()
 
-	testConnectorConfig := &GcpCloudSqlConnectorConfiguration{
-		DatabaseProtocol:   DatabaseProtocolMySql,
-		AuthenticationType: DatabaseAuthenticationTypeUsernameAndPassword,
-		UsernameAndPasswordAuth: &GcpCloudSqlUsernameAndPasswordAuthConfiguration{
-			UsernameAndPassword: UsernameAndPassword{
-				Username: "username",
-				Password: "password",
-			},
-			InstanceId:         "instance-id",
-			GcpCredentialsJson: `{"something": "something"}`,
+	testPasswordAuth := &DatabaseUsernameAndPasswordAuthConfiguration{
+		UsernameAndPassword: UsernameAndPassword{
+			Username: "username",
+			Password: "password",
 		},
 	}
-	testStandardConfig := &GcpCloudSqlStandardConfiguration{
-		DatabaseProtocol: DatabaseProtocolMySql,
-		HostnameAndPort: HostnameAndPort{
-			Hostname: "hostname",
-			Port:     3306,
-		},
-		AuthenticationType: DatabaseAuthenticationTypeTls,
-		TlsAuth: &DatabaseTlsAuthConfiguration{
-			UsernameAndPassword: UsernameAndPassword{
-				Username: "username",
-				Password: "password",
-			},
-			TlsConfig: TlsConfig{
-				Certificate: "certificate",
-				Key:         "private-key",
-			},
-		},
+
+	testConnectorAuthConfig := &GcpCloudSqlConnectorAuthConfiguration{
+		Username:           "username",
+		Password:           "password",
+		InstanceId:         "instance-id",
+		GcpCredentialsJson: `{"something": "something"}`,
+	}
+
+	testConnectorIamAuthConfig := &GcpCloudSqlConnectorIamAuthConfiguration{
+		Username:           "username",
+		InstanceId:         "instance-id",
+		GcpCredentialsJson: `{"something": "something"}`,
 	}
 
 	tests := []struct {
@@ -484,293 +468,53 @@ func Test_GcpCloudSqlDatabaseServiceConfiguration_Validate(t *testing.T) {
 		want  error
 	}{
 		{
-			name: "when connector is enabled, standard config should be nil",
-			given: GcpCloudSqlDatabaseServiceConfiguration{
-				CloudSqlConnectorEnabled: true,
-				Connector:                testConnectorConfig,
-				Standard:                 testStandardConfig,
-			},
-			want: errors.New("cloudsql_connector_enabled is true, but standard configuration is provided"),
-		},
-		{
-			name: "connector is enabled, but connector config is missing",
-			given: GcpCloudSqlDatabaseServiceConfiguration{
-				CloudSqlConnectorEnabled: true,
-				// connector config is missing
-			},
-			want: errors.New("Google Cloud SQL connector configuration is required"),
-		},
-		{
-			name: "when connector is disabled, connector config should be nil",
-			given: GcpCloudSqlDatabaseServiceConfiguration{
-				CloudSqlConnectorEnabled: false,
-				Connector:                testConnectorConfig,
-				Standard:                 testStandardConfig,
-			},
-			want: errors.New("cloudsql_connector_enabled is false, but connector configuration is provided"),
-		},
-		{
-			name: "connector is disabled, but standard config is missing",
-			given: GcpCloudSqlDatabaseServiceConfiguration{
-				CloudSqlConnectorEnabled: false,
-				// standard config is missing
-			},
-			want: errors.New("standard Google Cloud SQL configuration is required"),
-		},
-		{
-			name: "happy path - connector config",
-			given: GcpCloudSqlDatabaseServiceConfiguration{
-				CloudSqlConnectorEnabled: true,
-				Connector:                testConnectorConfig,
-			},
-			want: nil,
-		},
-		{
-			name: "happy path - standard config",
-			given: GcpCloudSqlDatabaseServiceConfiguration{
-				CloudSqlConnectorEnabled: false,
-				Standard:                 testStandardConfig,
-			},
-			want: nil,
-		},
-	}
-
-	for _, test := range tests {
-		test := test
-		t.Run(test.name, func(t *testing.T) {
-			t.Parallel()
-
-			err := test.given.Validate()
-			assert.Equal(t, test.want, err)
-		})
-	}
-}
-
-func Test_GcpCloudSqlStandardConfiguration_Validate(t *testing.T) {
-	t.Parallel()
-
-	testDatbaseProtocol := DatabaseProtocolMySql
-	testHostnameAndPort := HostnameAndPort{
-		Hostname: "hostname",
-		Port:     3306,
-	}
-	testPasswordAuth := &DatabaseUsernameAndPasswordAuthConfiguration{
-		UsernameAndPassword: UsernameAndPassword{
-			Username: "username",
-			Password: "password",
-		},
-	}
-	testTlsAuth := &DatabaseTlsAuthConfiguration{
-		UsernameAndPassword: UsernameAndPassword{
-			Username: "username",
-			Password: "password",
-		},
-		TlsConfig: TlsConfig{
-			Certificate: "certificate",
-			Key:         "private-key",
-		},
-	}
-
-	tests := []struct {
-		name  string
-		given GcpCloudSqlStandardConfiguration
-		want  error
-	}{
-		{
-			name:  "database protocol is missing",
-			given: GcpCloudSqlStandardConfiguration{
-				// database protocol is missing
-			},
-			want: errors.New("database protocol is required"),
-		},
-		{
 			name: "hostname is missing",
-			given: GcpCloudSqlStandardConfiguration{
-				DatabaseProtocol: testDatbaseProtocol,
+			given: GcpCloudSqlDatabaseServiceConfiguration{
 				HostnameAndPort: HostnameAndPort{
 					// hostname is missing
 					Port: 3306,
 				},
-				AuthenticationType:      DatabaseAuthenticationTypeUsernameAndPassword,
 				UsernameAndPasswordAuth: testPasswordAuth,
 			},
 			want: errors.New("hostname is a required field"),
 		},
 		{
 			name: "invalid database authentication type",
-			given: GcpCloudSqlStandardConfiguration{
-				DatabaseProtocol:   testDatbaseProtocol,
-				HostnameAndPort:    testHostnameAndPort,
-				AuthenticationType: "invalid",
+			given: GcpCloudSqlDatabaseServiceConfiguration{
+				GcpCloudSQLConnectorAuth:    testConnectorAuthConfig,
+				GcpCloudSQLConnectorIAMAuth: testConnectorIamAuthConfig,
 			},
-			want: errors.New("invalid database authentication type: invalid"),
+			want: errors.New("authentication type is cloudsql_auth_configuration, but username_and_password_auth_configuration, tls_auth_configuration or cloudsql_iam_auth_configuration is provided"),
 		},
 		{
-			name: "when username and password auth is picked, tls auth config should be nil",
-			given: GcpCloudSqlStandardConfiguration{
-				DatabaseProtocol:        testDatbaseProtocol,
-				HostnameAndPort:         testHostnameAndPort,
-				AuthenticationType:      DatabaseAuthenticationTypeUsernameAndPassword,
-				UsernameAndPasswordAuth: testPasswordAuth,
-				TlsAuth:                 testTlsAuth,
+			name: "cloudsql auth without password",
+			given: GcpCloudSqlDatabaseServiceConfiguration{
+				GcpCloudSQLConnectorAuth: &GcpCloudSqlConnectorAuthConfiguration{
+					Username: "username",
+				},
 			},
-			want: errors.New("authentication type is username_and_password, but TLS auth configuration is provided"),
+			want: errors.New("password is required"),
 		},
 		{
-			name: "username and password auth is picked, but username and password auth config is missing",
-			given: GcpCloudSqlStandardConfiguration{
-				DatabaseProtocol:   testDatbaseProtocol,
-				HostnameAndPort:    testHostnameAndPort,
-				AuthenticationType: DatabaseAuthenticationTypeUsernameAndPassword,
-				// username and password auth config is missing
+			name: "cloudsql auth without instance id",
+			given: GcpCloudSqlDatabaseServiceConfiguration{
+				GcpCloudSQLConnectorIAMAuth: &GcpCloudSqlConnectorIamAuthConfiguration{
+					Username: "username",
+				},
 			},
-			want: errors.New("username and password auth configuration is required"),
+			want: errors.New("instance ID is required"),
 		},
 		{
-			name: "when tls auth is picked, username and password auth config should be nil",
-			given: GcpCloudSqlStandardConfiguration{
-				DatabaseProtocol:        testDatbaseProtocol,
-				HostnameAndPort:         testHostnameAndPort,
-				AuthenticationType:      DatabaseAuthenticationTypeTls,
-				UsernameAndPasswordAuth: testPasswordAuth,
-				TlsAuth:                 testTlsAuth,
-			},
-			want: errors.New("authentication type is tls, but username and password auth configuration is provided"),
-		},
-		{
-			name: "tls auth is picked, but tls auth config is missing",
-			given: GcpCloudSqlStandardConfiguration{
-				DatabaseProtocol:   testDatbaseProtocol,
-				HostnameAndPort:    testHostnameAndPort,
-				AuthenticationType: DatabaseAuthenticationTypeTls,
-				// tls auth config is missing
-			},
-			want: errors.New("TLS auth configuration is required"),
-		},
-		{
-			name: "happy path - username and password auth",
-			given: GcpCloudSqlStandardConfiguration{
-				DatabaseProtocol:        testDatbaseProtocol,
-				HostnameAndPort:         testHostnameAndPort,
-				AuthenticationType:      DatabaseAuthenticationTypeUsernameAndPassword,
-				UsernameAndPasswordAuth: testPasswordAuth,
+			name: "happy path - cloudsql",
+			given: GcpCloudSqlDatabaseServiceConfiguration{
+				GcpCloudSQLConnectorAuth: testConnectorAuthConfig,
 			},
 			want: nil,
 		},
 		{
-			name: "happy path - tls auth",
-			given: GcpCloudSqlStandardConfiguration{
-				DatabaseProtocol:   testDatbaseProtocol,
-				HostnameAndPort:    testHostnameAndPort,
-				AuthenticationType: DatabaseAuthenticationTypeTls,
-				TlsAuth:            testTlsAuth,
-			},
-			want: nil,
-		},
-	}
-
-	for _, test := range tests {
-		test := test
-		t.Run(test.name, func(t *testing.T) {
-			t.Parallel()
-
-			err := test.given.Validate()
-			assert.Equal(t, test.want, err)
-		})
-	}
-}
-
-func Test_GcpCloudSqlConnectorConfiguration_Validate(t *testing.T) {
-	t.Parallel()
-
-	testDatbaseProtocol := DatabaseProtocolMySql
-	testPasswordAuth := &GcpCloudSqlUsernameAndPasswordAuthConfiguration{
-		UsernameAndPassword: UsernameAndPassword{
-			Username: "username",
-			Password: "password",
-		},
-		InstanceId:         "instance-id",
-		GcpCredentialsJson: `{"something": "something"}`,
-	}
-	testIamAuth := &GcpCloudSqlIamAuthConfiguration{
-		Username:           "username",
-		InstanceId:         "instance-id",
-		GcpCredentialsJson: `{"something": "something"}`,
-	}
-
-	tests := []struct {
-		name  string
-		given GcpCloudSqlConnectorConfiguration
-		want  error
-	}{
-		{
-			name:  "database protocol is missing",
-			given: GcpCloudSqlConnectorConfiguration{
-				// database protocol is missing
-			},
-			want: errors.New("database protocol is required"),
-		},
-		{
-			name: "invalid database authentication type",
-			given: GcpCloudSqlConnectorConfiguration{
-				DatabaseProtocol:   testDatbaseProtocol,
-				AuthenticationType: "invalid",
-			},
-			want: errors.New("invalid database authentication type: invalid"),
-		},
-		{
-			name: "when username and password auth is picked, iam auth config should be nil",
-			given: GcpCloudSqlConnectorConfiguration{
-				DatabaseProtocol:        testDatbaseProtocol,
-				AuthenticationType:      DatabaseAuthenticationTypeUsernameAndPassword,
-				UsernameAndPasswordAuth: testPasswordAuth,
-				IamAuth:                 testIamAuth,
-			},
-			want: errors.New("authentication type is username_and_password, but IAM auth configuration is provided"),
-		},
-		{
-			name: "username and password auth is picked, but username and password auth config is missing",
-			given: GcpCloudSqlConnectorConfiguration{
-				DatabaseProtocol:   testDatbaseProtocol,
-				AuthenticationType: DatabaseAuthenticationTypeUsernameAndPassword,
-				// username and password auth config is missing
-			},
-			want: errors.New("username and password auth configuration is required"),
-		},
-		{
-			name: "when iam auth is picked, username and password auth config should be nil",
-			given: GcpCloudSqlConnectorConfiguration{
-				DatabaseProtocol:        testDatbaseProtocol,
-				AuthenticationType:      DatabaseAuthenticationTypeIam,
-				UsernameAndPasswordAuth: testPasswordAuth,
-				IamAuth:                 testIamAuth,
-			},
-			want: errors.New("authentication type is iam, but username and password auth configuration is provided"),
-		},
-		{
-			name: "iam auth is picked, but iam auth config is missing",
-			given: GcpCloudSqlConnectorConfiguration{
-				DatabaseProtocol:   testDatbaseProtocol,
-				AuthenticationType: DatabaseAuthenticationTypeIam,
-				// iam auth config is missing
-			},
-			want: errors.New("IAM auth configuration is required"),
-		},
-		{
-			name: "happy path - username and password auth",
-			given: GcpCloudSqlConnectorConfiguration{
-				DatabaseProtocol:        testDatbaseProtocol,
-				AuthenticationType:      DatabaseAuthenticationTypeUsernameAndPassword,
-				UsernameAndPasswordAuth: testPasswordAuth,
-			},
-			want: nil,
-		},
-		{
-			name: "happy path - iam auth",
-			given: GcpCloudSqlConnectorConfiguration{
-				DatabaseProtocol:   testDatbaseProtocol,
-				AuthenticationType: DatabaseAuthenticationTypeIam,
-				IamAuth:            testIamAuth,
+			name: "happy path - cloudsql with IAM",
+			given: GcpCloudSqlDatabaseServiceConfiguration{
+				GcpCloudSQLConnectorIAMAuth: testConnectorIamAuthConfig,
 			},
 			want: nil,
 		},
@@ -1067,153 +811,6 @@ func Test_AwsRdsIamAuthConfiguration_Validate(t *testing.T) {
 			} else {
 				assert.EqualError(t, test.want, err.Error())
 			}
-		})
-	}
-}
-
-func Test_GcpCloudSqlUsernameAndPasswordAuthConfiguration_Validate(t *testing.T) {
-	t.Parallel()
-
-	testUsername := "username"
-	testPassword := "password"
-	testInstanceId := "instance-id"
-	testGcpCredentialsJson := `{"something": "something"}`
-
-	tests := []struct {
-		name  string
-		given GcpCloudSqlUsernameAndPasswordAuthConfiguration
-		want  error
-	}{
-		{
-			name: "username is missing",
-			given: GcpCloudSqlUsernameAndPasswordAuthConfiguration{
-				UsernameAndPassword: UsernameAndPassword{
-					// username is missing
-					Password: testPassword,
-				},
-				InstanceId:         testInstanceId,
-				GcpCredentialsJson: testGcpCredentialsJson,
-			},
-			want: errors.New("username is required"),
-		},
-		{
-			name: "password is missing",
-			given: GcpCloudSqlUsernameAndPasswordAuthConfiguration{
-				UsernameAndPassword: UsernameAndPassword{
-					Username: testUsername,
-					// password is missing
-				},
-				InstanceId:         testInstanceId,
-				GcpCredentialsJson: testGcpCredentialsJson,
-			},
-			want: errors.New("password is required"),
-		},
-		{
-			name: "instance id is missing",
-			given: GcpCloudSqlUsernameAndPasswordAuthConfiguration{
-				UsernameAndPassword: UsernameAndPassword{
-					Username: testUsername,
-					Password: testPassword,
-				},
-				// instance id is missing
-				GcpCredentialsJson: testGcpCredentialsJson,
-			},
-			want: errors.New("instance ID is required"),
-		},
-		{
-			name: "gcp credentials json is missing",
-			given: GcpCloudSqlUsernameAndPasswordAuthConfiguration{
-				UsernameAndPassword: UsernameAndPassword{
-					Username: testUsername,
-					Password: testPassword,
-				},
-				InstanceId: testInstanceId,
-				// gcp credentials json is missing
-			},
-			want: errors.New("GCP credentials JSON is required"),
-		},
-		{
-			name: "happy path",
-			given: GcpCloudSqlUsernameAndPasswordAuthConfiguration{
-				UsernameAndPassword: UsernameAndPassword{
-					Username: testUsername,
-					Password: testPassword,
-				},
-				InstanceId:         testInstanceId,
-				GcpCredentialsJson: testGcpCredentialsJson,
-			},
-			want: nil,
-		},
-	}
-
-	for _, test := range tests {
-		test := test
-		t.Run(test.name, func(t *testing.T) {
-			t.Parallel()
-
-			err := test.given.Validate()
-			assert.Equal(t, test.want, err)
-		})
-	}
-}
-
-func Test_GcpCloudSqlIamAuthConfiguration_Validate(t *testing.T) {
-	t.Parallel()
-
-	testUsername := "username"
-	testInstanceId := "instance-id"
-	testGcpCredentialsJson := `{"something": "something"}`
-
-	tests := []struct {
-		name  string
-		given GcpCloudSqlIamAuthConfiguration
-		want  error
-	}{
-		{
-			name: "username is missing",
-			given: GcpCloudSqlIamAuthConfiguration{
-				// username is missing
-				InstanceId:         testInstanceId,
-				GcpCredentialsJson: testGcpCredentialsJson,
-			},
-			want: errors.New("username is required"),
-		},
-		{
-			name: "instance id is missing",
-			given: GcpCloudSqlIamAuthConfiguration{
-				Username: testUsername,
-				// instance id is missing
-				GcpCredentialsJson: testGcpCredentialsJson,
-			},
-			want: errors.New("instance ID is required"),
-		},
-		{
-			name: "gcp credentials json is missing",
-			given: GcpCloudSqlIamAuthConfiguration{
-				Username:   testUsername,
-				InstanceId: testInstanceId,
-				// gcp credentials json is missing
-			},
-			want: errors.New("GCP credentials JSON is required"),
-		},
-		{
-			name: "happy path",
-			given: GcpCloudSqlIamAuthConfiguration{
-				Username:           testUsername,
-				InstanceId:         testInstanceId,
-				GcpCredentialsJson: testGcpCredentialsJson,
-			},
-			want: nil,
-		},
-	}
-
-	for _, test := range tests {
-		test := test
-		t.Run(test.name, func(t *testing.T) {
-			t.Parallel()
-
-			err := test.given.Validate()
-			assert.Equal(t, test.want, err)
 		})
 	}
 }
