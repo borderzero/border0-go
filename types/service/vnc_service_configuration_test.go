f0de496fe7a1a4f448450bfe8eb5287511c4b1de
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2024-03-15 15:05:44 -0700

    [ME-2704] Add Password to VNC Upstream Config (#93)

--- types/service/vnc_service_configuration_test.go
+++ types/service/vnc_service_configuration_test.go
@@ -18,16 +18,33 @@ func Test_ValidateVncServiceConfiguration(t *testing.T) {
 		{
 			name: "Happy case for vnc service",
 			configuration: &VncServiceConfiguration{
-				HostnameAndPort{Hostname: "hello.com", Port: 443},
+				HostnameAndPort: HostnameAndPort{Hostname: "hello.com", Port: 443},
+				Password:        "password",
+			},
+			expectedError: nil,
+		},
+		{
+			name: "Happy case for vnc service with empty password",
+			configuration: &VncServiceConfiguration{
+				HostnameAndPort: HostnameAndPort{Hostname: "hello.com", Port: 443},
 			},
 			expectedError: nil,
 		},
 		{
 			name: "Should fail for vnc service with invalid hostname/port (missing port)",
 			configuration: &VncServiceConfiguration{
-				HostnameAndPort{Hostname: "hello.com"},
+				HostnameAndPort: HostnameAndPort{Hostname: "hello.com"},
+				Password:        "password",
 			},
 			expectedError: fmt.Errorf("invalid hostname or port: port is a required field"),
+		},
+		{
+			name: "Should fail for vnc service with invalid password (too long)",
+			configuration: &VncServiceConfiguration{
+				HostnameAndPort: HostnameAndPort{Hostname: "hello.com", Port: 5900},
+				Password:        "longer-password",
+			},
+			expectedError: fmt.Errorf("password can be at most %d characters long", maxVncPasswordLength),
 		},
 	}
 	for _, test := range tests {
