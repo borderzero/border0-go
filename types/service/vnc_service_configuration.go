3695f9f9a32a10b14089b19b3959a0158ff740e8
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2024-03-15 14:11:42 -0700

    [ME-2704] Add Password to VNC Upstream Config

--- types/service/vnc_service_configuration.go
+++ types/service/vnc_service_configuration.go
@@ -2,16 +2,24 @@ package service
 
 import "fmt"
 
+const (
+	maxVncPasswordLength = 8
+)
+
 // VncServiceConfiguration represents service
 // configuration for vnc services (fka sockets).
 type VncServiceConfiguration struct {
 	HostnameAndPort
+	Password string `json:"password"`
 }
 
 // Validate validates the VncServiceConfiguration.
 func (c *VncServiceConfiguration) Validate() error {
 	if err := c.HostnameAndPort.Validate(); err != nil {
 		return fmt.Errorf("invalid hostname or port: %v", err)
+	}
+	if len(c.Password) > 8 {
+		return fmt.Errorf("password can be at most %d characters long", maxVncPasswordLength)
 	}
 	return nil
 }
