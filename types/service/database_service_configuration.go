b7df72dbaeb7bf17bb17df2a3edc3c9b0381a571
Pedro Andrade <pedro.rjandrade@gmail.com>
2025-08-08 19:38:53 -0300

    Change DatabaseNameOverride to DatabaseName (#174)
    - database name is best naming standard

--- types/service/database_service_configuration.go
+++ types/service/database_service_configuration.go
@@ -76,7 +76,7 @@ const (
 // - standard (when database service type is standard)
 //     - hostname and port
 //     - database protocol: mysql, postgres
-//     - database_name_override (optional)
+//     - database_name (optional)
 //     - authentication type: username_and_password, tls
 //     - username and password auth (when authentication type is username_and_password)
 //         - username
@@ -100,7 +100,7 @@ const (
 // - aws rds (when database service type is aws_rds)
 //     - hostname and port
 //     - database protocol: mysql, postgres
-//     - database_name_override (optional)
+//     - database_name (optional)
 //     - authentication type: username_and_password, tls
 //     - username and password auth (when authentication type is username_and_password)
 //         - username
@@ -116,7 +116,7 @@ const (
 //     - standard (when cloudsql_connector_enabled is false)
 //         - hostname and port
 //         - database protocol: mysql
-//         - database_name_override (optional)
+//         - database_name (optional)
 //         - authentication type: username_and_password, tls
 //         - username and password auth (when authentication type is username_and_password)
 //             - username
@@ -129,7 +129,7 @@ const (
 //             - key
 //     - connector (when cloudsql_connector_enabled is true)
 //         - database protocol: mysql, postgres
-//         - database_name_override (optional)
+//         - database_name (optional)
 //         - authentication type: username_password, iam
 //	   - username and password auth (when authentication type is username_and_password)
 //             - username
@@ -238,7 +238,7 @@ type StandardDatabaseServiceConfiguration struct {
 	DatabaseProtocol   string `json:"protocol"`
 	AuthenticationType string `json:"authentication_type"`
 
-	DatabaseNameOverride string `json:"database_name_override,omitempty"`
+	DatabaseName string `json:"database_name,omitempty"`
 
 	UsernameAndPasswordAuth *DatabaseUsernameAndPasswordAuthConfiguration `json:"username_and_password_auth_configuration,omitempty"`
 	TlsAuth                 *DatabaseTlsAuthConfiguration                 `json:"tls_auth_configuration,omitempty"`
@@ -343,7 +343,7 @@ type AwsRdsDatabaseServiceConfiguration struct {
 	DatabaseProtocol   string `json:"protocol"`
 	AuthenticationType string `json:"authentication_type"`
 
-	DatabaseNameOverride string `json:"database_name_override,omitempty"`
+	DatabaseName string `json:"database_name,omitempty"`
 
 	UsernameAndPasswordAuth *AwsRdsUsernameAndPasswordAuthConfiguration `json:"username_and_password_auth_configuration,omitempty"`
 	IamAuth                 *AwsRdsIamAuthConfiguration                 `json:"iam_auth_configuration,omitempty"`
@@ -390,9 +390,9 @@ func (config AwsRdsDatabaseServiceConfiguration) Validate() error {
 type AwsDocumentDBDatabaseServiceConfiguration struct {
 	HostnameAndPort
 
-	DatabaseProtocol     string `json:"protocol"`
-	AuthenticationType   string `json:"authentication_type"`
-	DatabaseNameOverride string `json:"database_name_override,omitempty"`
+	DatabaseProtocol   string `json:"protocol"`
+	AuthenticationType string `json:"authentication_type"`
+	DatabaseName       string `json:"database_name,omitempty"`
 
 	UsernameAndPasswordAuth *AwsRdsUsernameAndPasswordAuthConfiguration `json:"username_and_password_auth_configuration,omitempty"`
 	IamAuth                 *MongoDBAWSAuthConfiguration                `json:"iam_auth_configuration,omitempty"`
